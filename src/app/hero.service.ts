8776024d3df6de137f119e5270c77ba2f7da7f22
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-05-29 17:38:56 -0300

    searching for a hero by name

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -64,7 +64,7 @@ export class HeroService {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
     .pipe(
       tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
-      catchError(this.handleError<Hero>('addHero'))
+      catchError(this.handleError<Hero>("addHero"))
     );
   }
 
@@ -75,6 +75,19 @@ export class HeroService {
     .pipe(
       tap(_ => this.log(`deleted hero id=${id}`)),
       catchError(this.handleError<Hero>("deleteHero"))
+    );
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if(!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`)
+    .pipe(
+      tap(x => x.length ?
+         this.log(`found heroes matching "${term}"`) :
+         this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>("searchHeroes", []))
     );
   }
 
