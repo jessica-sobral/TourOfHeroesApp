2500afe1862f5b18e6cafc37f2bb6478f33b5c2a
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-05-29 16:44:41 -0300

    saving hero after updating name

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -48,4 +48,16 @@ export class HeroService {
     };
   }
 
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
+    .pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>("updateHero"))
+    );
+  }
+
+  httpOptions = {
+    headers: new HttpHeaders({ "Content-Type": "application/json"})
+  };
+
 }
