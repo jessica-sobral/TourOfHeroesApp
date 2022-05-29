3d361bd43795a3a345a656de23af7f1356a6ad6e
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-05-29 16:30:22 -0300

    handling errors with catchError

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from "rxjs";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
@@ -17,7 +18,11 @@ export class HeroService {
   ) { }
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(_ => this.log("fetched heroes")),
+      catchError(this.handleError<Hero[]>("getHeroes", []))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
@@ -31,5 +36,13 @@ export class HeroService {
   }
 
   private heroesUrl = "api/heroes";
+
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
 
 }
